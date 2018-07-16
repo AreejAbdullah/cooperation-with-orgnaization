b23a6fe59cbbfdf6da85a06cf257b2f85c88549e
areejabdullah <areej_ak@hotmail.com>
2018-07-16 14:26:18 +0300

    add

---
+++ A.php
@@ -0,0 +1,524 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+
+<style  type="text/css">
+ body {
+    background-color: #f0f0f0;
+}
+@font-face{
+font-family:Neo Sans Arabic Regular; 
+src:url("Neo Sans Arabic Regular.ttf");
+
+
+}
+
+ul {
+    list-style-type: none;
+    margin: 10px 0px 0px 0px ;
+    padding: 0px;
+    overflow: hidden;
+    background-color: rgb(69, 66, 154) ;
+}
+
+.H {
+    float: left;
+font-family: Neo Sans Arabic Regular;
+}
+.V{
+
+float: right;
+font-size:17px;
+font-family: Neo Sans Arabic Regular;
+}
+
+
+li a {
+    display: block;
+    color: LightGray;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+   color: white ;
+}
+
+div {
+
+display : block ;
+
+}
+
+.active {
+    background-color: rgb(33, 186, 166) ;
+        color: white;
+
+}
+
+.pic{
+    text-align:right;
+    background-color: white;
+    padding: 0px 50px 50px 50px;
+
+	
+}
+
+.innerPages .mainContent {
+    padding-top: 30px;
+    padding-bottom: 30px;
+    overflow: hidden;
+}
+
+
+
+
+
+.D{
+position: relative;
+    background-color: #fff;
+    width: 700px;
+height:500px;
+    padding: 0px;
+    margin: 0px 120px 50px 50px;
+float: left ;
+
+
+}
+.E{
+
+    background-color: #f0f0f0;
+    width: 300px;
+    padding: 25px;
+    margin: 25px;
+    float: right ;
+}
+.T{
+    list-style-type: none;
+    padding: 0px;
+    overflow: hidden;
+    background-color: rgb(158, 172, 193) ;
+    margin: 0px 0px 0px 0px ;
+
+
+}
+.f{
+    list-style-type: none;
+    overflow: hidden;
+    background-color: rgb(33, 186, 166) ;
+    margin: 60px 0px 0px 0px ;
+    padding-top: 50px;
+  
+		
+		
+		
+
+
+}
+.G{
+position: absolute;
+ top:50px;
+  left: 50px;
+ right: 50px;
+width: 600px;
+height:400px;
+background-color: #f0f0f0 ;
+overflow: auto;
+ border-radius: 5px;
+
+}
+.A{ 
+    color: while;
+    padding: 14px 20px;
+    margin: 10px 0;
+    font-size:20px;
+text-align: right;
+float: right ;
+
+}
+.K{
+background-color:white;
+position:relative;
+height:100px;
+margin-top:-100px;
+clear:both;
+
+
+}
+li.dropdown {
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+.footer{
+background-color:white;
+position:relative;
+height:100px;
+margin-top:-100px;
+clear:both;
+
+
+}
+.O{
+background-color:white;
+
+word-spacing: 20px;
+font-size:10px;
+color: rgb(153, 166, 198) ;
+list-style-type: none;
+
+}
+
+</style>
+</head>
+<body>
+
+<ul >
+ <li style="float:left ; margin-left:16px;"><a class="active" href="#about"><span class="fas fa-search"></span></a></li>
+    <li class="H"><a href="#English">English <span class="fa fa-outdent"></span></a></li>
+	  
+	  
+	  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+
+	<li class="H" ><a href="#account">الصفحة الشخصية <span class="fas fa-user-alt"></span></a></li>
+<li class="H" ><a href="#contact">بريد الموظفين   <span class="far fa-envelope"></span></a></li>
+
+</ul>
+
+
+
+
+  <div class="pic"><img src="logo.png" alt="logo" ></div>
+<ul class="T">
+<li class="V" style=" margin: 0px 50px 0px 0px;"><a href="#contact">الرئيسية</a></li>
+<li class="V" ><a href="#contact">    الهيئة                    </a></li>
+<li class="V" ><a href="#contact">    المكتبة الإحصائية          </a></li>
+<li class="V" ><a href="#contact">    الخدمات الإلكترونية        </a></li>
+<li class="V" ><a href="#contact">    الأدلة و التصنيفات الإحصائية</a></li>
+<li class="V" ><a href="#account">    المركز الإعلامي             </a></li>
+<li class="V" ><a href="#news">       التوظيف                   </a></li>
+<li class="V" ><a href="#English">    المسوح الميدانية          </a></li>
+<li class="V"><a  href="#about">      اتصل بنا                  </a></li>
+
+</ul>
+
+<div class="f">
+  
+
+</div>
+
+
+
+<div class="E">
+<img class="card-img-top" src="logo.png" alt="Card image cap"><br><br>    
+<a style= "float: center; width:100%;" href="/ar/profile-registration_data/30862/edit" class="custom-width btn btn-primary">معلومات الحساب</a><br><br>
+<a style= "float: center; width:100%;" href="/ar/user/30862/edit" class="custom-width btn btn-primary">الإعدادات</a><br><br>
+<a style= "float: center; width:100%; background-color:darkblue;" href="/ar/node/add/information-request" class="custom-width btn btn-primary">الوثائق</a><br><br>
+<a style= "float: center; width:100%;" href="/ar/user/30862/bookmarks" class="custom-width btn btn-primary">طلب معلومةإحصائية</a><br><br>
+<a style= "float: center; width:100%;" href="/ar/user/30862/bookmarks" class="custom-width btn btn-primary">الإشتراكات</a><br><br>
+</div>
+
+<form action="a.php" method="post" enctype="multipart/form-data">
+<div class="D">
+
+<div class="G">
+<br>
+
+<br><p>
+<label class= "A" for="file" >: أضف الوثيقة</label><br><br>
+<input class= "A" type="file" name="file" id="file"  /> <br><br>
+<input  style="text-align: right;float: right ;  margin: 50px 320px 50px 500px;" type="submit" name="submit" value="إضافة" class="btn btn-outline-success" />
+
+<br/></p>
+
+
+
+<br><br><br>
+
+
+    <?php
+$connection = mysql_connect("localhost","root","");
+if(! $connection) {
+die("connection failed" .mysql_error());
+
+}
+$db = mysql_select_db( "account gafs",$connection ) ;
+if(! $db) {
+
+die("database failed" .mysql_error());
+
+}
+if(isset($_POST["submit"])){
+
+$l = $_FILES["file"]["name"] ;
+
+
+ $query ="INSERT INTO documents (image) VALUES ('$l')";
+
+
+
+$result = mysql_query( $query , $connection);
+if($result==1){
+echo"تمت إضافة الوثيقة";
+}
+else{
+echo"لم تتم إضافة الوثيقة";
+}
+}
+
+?>
+<?php
+
+$conn = mysql_connect("localhost","root","") ; 
+
+$db = mysql_select_db( "account GAFS",$conn ); 
+
+$query2 ="Select * from documents ";
+
+
+$result2 = mysql_query($query2, $conn);
+
+
+mysql_close($conn);
+
+
+?>
+<table>
+<?php
+
+$conn = mysql_connect("localhost","root","") ; 
+
+$db = mysql_select_db( "account GAFS",$conn ); 
+
+
+while($row = mysql_fetch_row($result2)){
+print("<tr>");
+
+foreach($row as $value)
+
+
+print("<td style='float: right;'><a href='delete.php?id=$row[0]' class='btn btn-outline-danger' >حذف</a></td>");
+print("<td style='float: right ;'><a href='select.php?id=$row[0]' class='btn btn-outline-primary' >تحديد</a></td>");
+print("<th><img src='$value' class='img-thumbnail' style='float: right;'>$value</th>");
+
+print("</tr>");
+}
+mysql_close($conn);
+
+
+?>
+</table>
+</form>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+
+<form action="a.php">
+
+<button type="button" class="btn btn-primary" id="myBtn"   >مشاركة</button>
+
+<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter">حذف الكل</button>
+
+
+
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle" >حذف الكل</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        هل انت متأكد من حذف جميع الوثائق؟
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">إلغاء</button>
+        <button type="button" class="btn btn-primary" onclick="f()">حذف</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+  </div>
+</div>
+
+
+
+
+</div>
+
+
+
+<footer class="footer">
+
+    <li style="margin-bottom: 11px; padding-top: 45px; padding-left: 40px; font-size:15px; " class="fab fa-linkedin-in"></li>
+    <li style="margin-bottom: 11px; padding-top: 45px; padding-left: 22px; font-size:15px; " class="fab fa-facebook-f"></li>
+    <li style="margin-bottom: 11px; padding-top: 45px; padding-left: 22px; font-size:15px; " class="fab fa-twitter"></li>
+	<li style="margin-bottom: 11px; padding-top: 45px; padding-left: 22px; font-size:15px; " class="fab fa-youtube"></li>
+    <li style="margin-bottom: 11px; padding-top: 45px; padding-left: 22px; font-size:15px; " class="fab fa-instagram"></li>
+    <li style="margin-bottom: 11px; padding-top: 45px; padding-left: 22px; font-size:15px; " class="fas fa-rss"></li>
+
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left:12px; padding-right: 40px; font-size:12px;"  >جميع الحقوق محفوظة للهيئة العامة للإحصاء © 2018</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left: 12px; font-size:12px;" >سياسة الخصوصية</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left: 12px; font-size:12px;">|</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left: 12px; font-size:12px;" >سياسة الاستخدام</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left: 12px; font-size:12px;">|</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none;padding-left:  12px; font-size:12px;" >خريطة الموقع</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left: 12px; font-size:12px;">|</li>
+<li style="float: right; margin-bottom: 11px; padding-top: 45px; list-style-type: none; padding-left: 12px; font-size:12px;" >اتصل بنا</li>
+
+
+
+ 
+
+
+
+
+
+</footer>
+<script>
+ function f(){
+ window.location="deleteall.php";
+ 
+ }
+
+
+</script>
+
+<div id="myModal" class="modal">
+<div class="modal-content">
+  
+    <div class="modal-header">
+    <h2> Documents to be sent: </h2> 
+    </div>
+ 
+ <?php
+
+$conn = mysql_connect("localhost","root","") ; 
+
+$db = mysql_select_db( "account GAFS",$conn ); 
+
+$query2 ="Select * from documentsselect ";
+
+
+$result2 = mysql_query($query2, $conn);
+
+
+mysql_close($conn);
+
+
+?>
+<table>
+<?php
+
+$conn = mysql_connect("localhost","root","") ; 
+
+$db = mysql_select_db( "account GAFS",$conn ); 
+
+
+
+while($row = mysql_fetch_row($result2)){
+print("<tr>");
+
+foreach($row as $value)
+
+print("<td style='float: right;'><a href='deleted.php?id=$row[0]' class='btn btn-outline-danger' >حذف</a></td>");
+
+print("<th><img src='$value' class='img-thumbnail' style='float: right;'></th>");
+
+
+print("</tr>");
+}
+mysql_close($conn);
+
+
+?>
+ 
+ 
+ 
+    <div class="modal-body">
+ 
+ 
+<p class="double"></p>
+
+ <form>
+SENT TO  : <input type ="text" name="Sent to" style="margin:4px;width:70%; border: 2px solid black;"><br>
+ADDRESS: <input type ="text" name="Address" style="margin:4px;width:70%; border: 2px solid black;"><br>
+<textarea rows="7" cols="60" placeholder="ADD COMMENT"> </textarea><br>
+<input type ="submit" value="SENT"><input type ="reset" value="BACK"><br>
+ </form>
+ </div> </div> </div>
+ 
+ 
+ <script>
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+
+
+</body>
+
+
+
+</html>
